e9a67e1c742cb7cfc3e0e4a015de58b1e8e081a5
Slasher <slesherator@gmail.com>
2026-05-13 07:09:57 +0300

    Добавлено chapter_02 и chapter_03

---
+++ chapter_02/vararg.cpp
@@ -0,0 +1,22 @@
+// передача переменных в функцию в качестве аргументов
+#include <iostream>
+using namespace std;
+
+void repchar (char ch, int n)
+{
+    for (int j=0; j<n; j++)
+    cout << ch;
+    cout << endl;
+}
+
+int main()
+{
+    char chin;
+    int nin;
+    cout << "Введите символ: ";
+    cin >> chin;
+    cout << "Введите число повторений символа: ";
+    cin >> nin;
+    repchar(chin, nin);
+    return 0;
+}
